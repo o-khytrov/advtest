3721f5f257de63c8ae255ab934288eea17246e38
O.Khytrov <o.khytrov@gmail.com>
2021-11-08 14:22:41 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as tf from '@tensorflow/tfjs'
+import { TestContext } from 'src/testContext';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'advtest';
+  testContext: TestContext;
+  model: tf.LayersModel;
+  dataset: any;
+  constructor() {
+    this.testContext = new TestContext();
+  }
+
+  @ViewChild('i_model') i_model: ElementRef;
+  @ViewChild('i_weights') i_weights: ElementRef;
+  @ViewChild('i_test_data') i_test_data: ElementRef;
+  @ViewChild('i_labels') i_labels: ElementRef;
+  @ViewChild('i_class_names') i_class_names: ElementRef;
+
+  async loadModel() {
+
+    this.model = await tf.loadLayersModel(tf.io.browserFiles([this.i_model.nativeElement.files[0], this.i_weights.nativeElement.files[0]]));
+
+    this.model.summary(null, null, (x) => {
+      this.testContext.summary = x;
+      console.log(this.testContext.summary);
+    });
+
+    var classNamesReader = new FileReader();
+    classNamesReader.onload = (e) => {
+      this.testContext.classNames = JSON.parse(classNamesReader.result.toString());
+    }
+
+    classNamesReader.readAsText(this.i_class_names.nativeElement.files[0]);
+
+  }
+  onFileChange(event) {
+    event.target.nextSibling.innerText = event.target.files[0].name;
+  }
+
+  loadDataset() {
+    // Load data in form [{xs: x0_tensor, ys: y0_tensor}, {xs: x1_tensor, ys: y1_tensor}, ...]
+    let x, y;
+    let loadingX = this.readFile(this.i_test_data.nativeElement.files[0]).then(arr => x = tf.data.array(arr as []).batch(1));
+    let loadingY = this.readFile(this.i_labels.nativeElement.files[0]).then(arr => y = tf.data.array(arr as []).batch(1));
+    let loadingData = Promise.all([loadingX, loadingY]).then(() => tf.data.zip([x, y]).toArray()).then(ds => this.dataset = ds.map(e => { return { xs: e[0], ys: e[1] } })).then(x => {
+      var img = this.dataset[0].xs;
+      var p = (this.model.predict(img)as tf.Tensor).dataSync();
+      console.log(p);
+    });
+  }
+
+  readFile(file: File) {
+    return new Promise((resolve, reject) => {
+      const fr = new FileReader();
+      fr.onerror = reject;
+      fr.onload = function () {
+        resolve(JSON.parse(fr.result.toString()));
+      }
+      fr.readAsText(file);
+    });
+  }
 }
---
+++ src/testContext.ts
@@ -0,0 +1,10 @@
+import { TypeFlags } from "typescript";
+import * as tf from '@tensorflow/tfjs'
+
+export class TestContext {
+    summary: Array<string>;
+    testData:Array<Array<Array<number>>>;
+    lables:Array<Array<number>>;
+    classNames:Array<string>;
+    images:Array<string>;
+}
